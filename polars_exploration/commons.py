ea92be758db9102131b7d5513334616f55e97e9d
rojoyin <rojoyin@gmail.com>
2025-03-14 13:04:55 -0500

    Put common functions in separate module

---
+++ polars_exploration/commons.py
@@ -0,0 +1,24 @@
+import time
+
+import requests
+
+
+def timer_decorator(func):
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.time()
+        value = func(*args, **kwargs)
+        end_time = time.time()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+def download_file(url, filename):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+    print(f"File downloaded successfully as {filename}")
--- polars_exploration/main.py
+++ polars_exploration/main.py
@@ -1,19 +1,13 @@
-import polars as pl
-import requests
 import os
+
+import polars as pl
+
+from polars_exploration.commons import download_file, timer_decorator
 
 url = "https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD"
 filename = "Electric_Vehicle_Population_Data.csv"
 
 
-def download_file(url, filename):
-    response = requests.get(url, stream=True)
-    response.raise_for_status()
-    
-    with open(filename, 'wb') as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            file.write(chunk)
-    print(f"File downloaded successfully as {filename}")
 
 if os.path.exists(filename):
     print(f"{filename} already exists.")
@@ -21,20 +15,8 @@ else:
     download_file(url, filename)
 
 
-import time
-
-def timer_decorator(func):
-    def wrapper_timer(*args, **kwargs):
-        start_time = time.time()
-        value = func(*args, **kwargs)
-        end_time = time.time()
-        run_time = end_time - start_time
-        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
-        return value
-    return wrapper_timer
-
 @timer_decorator
-def check_file(filename):
+def check_file_with_polars(filename):
     electric_vehicle_data = pl.read_csv(filename)
     print("First few rows of the data:")
     print(electric_vehicle_data.head())
@@ -47,4 +29,4 @@ def check_file(filename):
     print(grouped_by_city)
 
 
-check_file(filename)
+check_file_with_polars(filename)
