5a940ae4b6d80f4da4abb04d455b26aa90ea9485
rojoyin <rojoyin@gmail.com>
2025-03-14 13:24:26 -0500

    Add function to run similar analysis using dask

--- polars_exploration/main.py
+++ polars_exploration/main.py
@@ -1,6 +1,7 @@
 import os
 
 import polars as pl
+import dask.dataframe as dd
 
 from polars_exploration.commons import download_file, timer_decorator
 
@@ -29,4 +30,25 @@ def check_file_with_polars(filename):
     print(grouped_by_city)
 
 
+@timer_decorator
+def check_file_with_dask(filename):
+    dtypes = {
+        '2020 Census Tract': 'float64',
+        'Base MSRP': 'float64',
+        'Electric Range': 'float64',
+        'Legislative District': 'float64',
+        'Postal Code': 'float64'
+    }
+    electric_vehicle_data = dd.read_csv(filename, dtype=dtypes)
+    print("First few rows of the data:")
+    print(electric_vehicle_data.head())
+    print(electric_vehicle_data.describe().compute())
+
+    grouped_by_city = electric_vehicle_data.groupby("City")["VIN (1-10)"].count().compute()
+    grouped_by_city = grouped_by_city.sort_values()
+    print("\nVehicles per city:")
+    print(grouped_by_city)
+
+
 check_file_with_polars(filename)
+check_file_with_dask(filename)
